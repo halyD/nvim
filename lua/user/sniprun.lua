5b4106532dd4c63a282ab7d542eaffa5e473249d
MihalyD <fmst.xx@hotmail.com.tw>
2022-05-08 17:04:02 +0800

    configs

--- lua/user/sniprun.lua
+++ lua/user/sniprun.lua
@@ -1,17 +1,57 @@
-local status_ok, neoscroll = pcall(require, "neoscroll")
+local status_ok, sniprun = pcall(require, "sniprun")
 if not status_ok then
-  return
+	return
 end
 
-neoscroll.setup {
-  -- All these keys will be mapped to their corresponding default scrolling animation
-  mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-  hide_cursor = true, -- Hide cursor while scrolling
-  stop_eof = true, -- Stop at <EOF> when scrolling downwards
-  use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-  respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  easing_function = nil, -- Default easing function
-  pre_hook = nil, -- Function to run before the scrolling animation starts
-  post_hook = nil, -- Function to run after the scrolling animation ends
+sniprun.setup {
+  -- selected_interpreters = {}, --# use those instead of the default for the current filetype
+  -- repl_enable = { "Python3_original" }, --# enable REPL-like behavior for the given interpreters
+  -- repl_disable = {}, --# disable REPL-like behavior for the given interpreters
+
+  -- interpreter_options = { --# intepreter-specific options, see docs / :SnipInfo <name>
+  --   GFM_original = {
+  --     use_on_filetypes = { "markdown.pandoc" }, --# the 'use_on_filetypes' configuration key is
+  --     --# available for every interpreter
+  --   },
+  -- },
+
+  --# you can combo different display modes as desired
+  display = {
+    -- "Classic", --# display results in the command-line  area
+    "VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
+    -- "VirtualTextErr", --# display error results as virtual text
+    -- "TempFloatingWindow",      --# display results in a floating window
+    -- "LongTempFloatingWindow",  --# same as above, but only long results. To use with VirtualText__
+    -- "Terminal",                --# display results in a vertical split
+    -- "TerminalWithCode",        --# display results and code history in a vertical split
+    -- "NvimNotify",              --# display with the nvim-notify plugin
+    -- "Api"                      --# return output to a programming interface
+  },
+
+  display_options = {
+    terminal_width = 45, --# change the terminal display option width
+    notification_timeout = 5, --# timeout for nvim_notify output
+  },
+
+  --# You can use the same keys to customize whether a sniprun producing
+  --# no output should display nothing or '(no output)'
+  show_no_output = {
+    "Classic",
+    "TempFloatingWindow", --# implies LongTempFloatingWindow, which has no effect on its own
+  },
+
+  --# customize highlight groups (setting this overrides colorscheme)
+  -- snipruncolors = {
+  --   SniprunVirtualTextOk = { bg = "NONE", fg = "#66eeff", ctermbg = "Black", cterfg = "Cyan" },
+  --   SniprunFloatingWinOk = { fg = "NONE", ctermfg = "Cyan" },
+  --   SniprunVirtualTextErr = { bg = "#881515", fg = "#000000", ctermbg = "DarkRed", cterfg = "Black" },
+  --   SniprunFloatingWinErr = { fg = "#881515", ctermfg = "DarkRed" },
+  -- },
+
+  --# miscellaneous compatibility/adjustement settings
+  inline_messages = 0, --# inline_message (0/1) is a one-line way to display messages
+  --# to workaround sniprun not being able to display anything
+
+  borders = "single", --# display borders around floating windows
+  --# possible values are 'none', 'single', 'double', or 'shadow'
 }
