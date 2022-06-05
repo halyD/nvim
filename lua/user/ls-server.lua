6f1940b649e6dfde84e416099e076a3ddc8afc98
MihalyD <fmst.xx@hotmail.com.tw>
2022-06-05 19:13:29 +0800

    update Lsp: server & key mappings

--- lua/user/ls-server.lua
+++
@@ -1,5 +0,0 @@
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.sumneko_lua.setup{}
-require'lspconfig'.rust_analyzer.setup{}
--- lua/user/lsp-config.lua
+++ lua/user/lsp-config.lua
@@ -1,11 +1,19 @@
-
 vim.api.nvim_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-k>', ':lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-n>', ':lua vim.lsp.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-p>', ':lua vim.lsp.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-n>', ':lua vim.lsp.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-p>', ':lua vim.lsp.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
 -- autocmd BufWritePre *.cpp <cmd>lua vim.lsp.buf.formatting_sync(nil, 100)
-vim.cmd('autocmd BufWritePost * lua vim.lsp.buf.formatting()')
+vim.cmd [[autocmd! BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 100)]]
+
+
+
+local servers = { 'pyright', 'rust_analyzer', 'bashls', 'sumneko_lua', 'clangd' }
+
+for _, lsp in pairs(servers) do
+	require('lspconfig')[lsp].setup {}
+
+end
